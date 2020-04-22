27a874bdc2c1da79d0d4324b4e4b75b1d6f6ba82
camila queiroz <queiroz.cami@outlook.com>
2020-04-22 16:04:57 -0300

    Adding rails yelp exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St", category: "belgian" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St", category: "italian" }
+japa_go =  { name: "Japa Go", address: "Djalma Ulrich 69 ", category: "japanese" }
+pettit_poa = { name: "Pettit Poa", address: "Algum lugar em paris", category: "french" }
+chinatown = { name: "China Town", address: "Chinguiling", category: "chinese"}
+pizza_west = { name: "Pizza West", address: "roma com amor", category: "italian"}
+
+[dishoom, pizza_east, japa_go, pettit_poa, chinatown, pizza_west].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
